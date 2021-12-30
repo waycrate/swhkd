05da95fe5bf24eac4298b831ab18f6e0d97c21d4
ckyiu <UnsignedArduino@outlook.com>
2021-12-30 17:02:35 -0500

    Refactor swhkd.py

--- src/swhkd.py
+++ src/swhkd.py
@@ -2,32 +2,37 @@
 import asyncio
 import getpass
 import grp
+import logging
 import os
 import pwd
 import signal
 import sys
+from json import JSONDecodeError
+from pathlib import Path
 
-from utils.log import LOG_UTILS
-from utils.input import INPUT_UTILS
-from utils.config import CONFIG_PARSER
+from utils.config import ConfigParser
+from utils.input import SWHKDHelper
+from utils.log import create_logger
+
+logger = create_logger(name=__name__, level=logging.DEBUG)
 
 
 class SWHKD:
     def __init__(self):
-        signal.signal(signal.SIGINT, self.signalhandler)
-        signal.signal(signal.SIGTERM, self.signalhandler)
-        self.log_util = LOG_UTILS()
-        self.input_util = INPUT_UTILS()
+        signal.signal(signal.SIGINT, SWHKD.signal_handler)
+        signal.signal(signal.SIGTERM, SWHKD.signal_handler)
+        self.input_util = SWHKDHelper()
         self.user = getpass.getuser()
-        self.config_parser = CONFIG_PARSER()
+        self.config_parser = ConfigParser()
 
-    def signalhandler(self,sig,frame):
-        print('\033[1;31mEXIT: Quitting SWHKD.\033[0m')
+    @staticmethod
+    def signal_handler(sig, frame):
+        logger.warning("Quitting SWHKD")
         sys.exit(0)
 
     async def run_swhkd(self):
         if os.getuid() == 0:
-            await self.log_util.log_error('Refusing to run SWHKD as root.')
+            logger.critical("Refusing to run SWHKD as root")
             sys.exit(1)
 
         # Permission check
@@ -35,26 +40,54 @@ class SWHKD:
         gid = pwd.getpwnam(self.user).pw_gid
         groups.append(grp.getgrgid(gid).gr_name)
         if "input" not in groups:
-                await self.log_util.log_error("User is in not in input group, exiting.")
-                sys.exit(1)
+            logger.error("User is in not in input group, exiting.")
+            sys.exit(1)
 
         # Config parsing
-        try:
-            config = await self.config_parser.parse("{0}swhkd/config.json".format(os.environ.get("XDG_CONFIG_HOME")))
-        except FileNotFoundError:
+        config_paths = [
+            Path("~/.config/swhkd/config.json")
+        ]
+
+        # Expand all the paths, (like convert ~ to /home/(username))
+        config_paths = [path.expanduser().resolve() for path in config_paths]
+
+        # Add XDG_CONFIG_HOME/swhkd/config.json if XDG_CONFIG_HOME is an
+        # environment variable
+        xdg_config_home = os.environ.get("XDG_CONFIG_HOME")
+        if xdg_config_home is not None:
+            config_home_path = Path(xdg_config_home) / "swhkd" / "config.json"
+            logger.debug(f"XDG_CONFIG_HOME: {xdg_config_home}")
+            logger.debug(f"New possible configuration path: "
+                         f"{config_home_path}")
+            config_paths.insert(0, config_home_path)
+        else:
+            logger.warning("XDG_CONFIG_HOME environment variable not set")
+
+        # Try every configuration file location
+        config = None
+        for path in config_paths:
             try:
-                config = await self.config_parser.parse("~/.config/swhkd/config.json")
+                config = await self.config_parser.parse(path)
             except FileNotFoundError:
-                await self.log_util.log_error("Failed to parse config files.")
-                sys.exit(1)
+                logger.debug(f"No file found at {path}")
+            except JSONDecodeError:
+                logger.warning(f"Invalid JSON file found at {path}")
+            else:
+                logger.info(f"Found configuration at {path}!")
+        if config is None:
+            logger.critical(f"No configuration files found! "
+                            f"(checked {len(config_paths)} location(s))")
+            sys.exit(1)
 
         # Fetch events
         keyboards = await self.input_util.get_keyboard_devices()
-        if not keyboards:
-            await self.log_util.log_error("No keyboard devices found.")
+        if len(keyboards) == 0:
+            logger.critical("No keyboard devices found!")
             sys.exit(1)
+
         for keyboard in keyboards:
             await self.input_util.get_keyboard_events(keyboard)
+
 
 if __name__ == "__main__":
     asyncio.run(SWHKD().run_swhkd())
