704d1f1e47aab2da11755351c1510a5d4dbc097d
ckyiu <UnsignedArduino@outlook.com>
2021-12-29 20:22:25 -0500

    Add requirements.txt and clean up configuration related stuff

--- src/utils/config.py
+++ src/utils/config.py
@@ -8,8 +8,19 @@ class CONFIG_PARSER:
 
     async def parse(self,config_file):
         output=[]
-        with open(config_file, 'r') as file:
-            contents = json.loads(file.read())
+        with open(config_file, 'rt') as file:
+            text = file.read()
+        # Remove comments
+        # Remove \r if found for some reason
+        text = text.replace("\r", "")
+        text_lines = text.split("\n")
+        cleaned_text_lines = []
+        for line in text_lines:
+            if not line.startswith("//"):
+                cleaned_text_lines.append(line)
+        cleaned_text = "\n".join(cleaned_text_lines).strip()
+        # Parse the text without lines beginning with //
+        contents = json.loads(cleaned_text)["config"]
         for arr in contents:
             pair=[]
             for key, value in arr.items():
