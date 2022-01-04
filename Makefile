8bbd34c4df42ad9be3f67cbe63e0f9c732c85b0f
Shinyzenith <aakashsensharma@gmail.com>
2022-01-05 00:18:08 +0530

    Check all event devices for KEY_ENTER presence.

---
+++ Makefile
@@ -0,0 +1,29 @@
+all: build
+
+build:
+	@cargo build --release --target=x86_64-unknown-linux-musl
+
+install:
+	@mkdir -p /usr/local/bin
+	@cp ./target/x86_64-unknown-linux-musl/release/swhkd /usr/local/bin/swhkd
+	@chmod +x /usr/local/bin/swhkd
+
+uninstall:
+	@rm /usr/local/bin/swhkd
+
+run:
+	@cargo run --target=x86_64-unknown-linux-musl
+
+check:
+	@cargo fmt
+	@cargo check --target=x86_64-unknown-linux-musl
+
+clean:
+	@cargo clean
+
+setup:
+	@rustup install stable
+	@rustup default stable
+	@rustup target add x86_64-unknown-linux-musl
+
+.PHONY: check clean setup all run install build
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use evdev::Device;
+use evdev::Key;
+use glob::glob;
+
+pub fn main() {
+    for entry in glob("/dev/input/event*").expect("Failed to read /dev/input/event*.") {
+        match entry {
+            Ok(path) => {
+                check_keyboard(path.into_os_string().into_string().unwrap());
+            }
+            Err(_) => continue,
+        }
+    }
+}
+
+pub fn check_keyboard(input_path: String) -> bool {
+    let device = Device::open(&input_path).expect("Failed to open device.");
+    if device
+        .supported_keys()
+        .map_or(false, |keys| keys.contains(Key::KEY_ENTER))
+    {
+        println!("{} is a keyboard.", input_path);
+        return true;
+    } else {
+        println!("{} is not a keyboard.", input_path);
+        return false;
+    }
+}
