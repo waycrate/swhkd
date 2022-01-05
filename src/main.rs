c774f6b3f8485aafd05cf52933b1eca5b80de292
Shinyzenith <aakashsensharma@gmail.com>
2022-01-05 19:44:47 +0530

    move input group permission check to separate function

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,8 @@ pub fn main() {
         exit(1);
     }
 
-    log::trace!("Checking if invoking user is in input group or not.");
-    let groups = unistd::getgroups();
-    let mut is_in_input_group: bool = false;
-    'init_loop: for (_, groups) in groups.iter().enumerate() {
-        for group in groups {
-            let group = unistd::Group::from_gid(*group);
-            if group.unwrap().unwrap().name == "input" {
-                log::debug!("Invoking user is in input group.");
-                is_in_input_group = true;
-                break 'init_loop;
-            }
-        }
-    }
-
-    if is_in_input_group == false {
+    /* Check if invoking user is in input group or not */
+    if user_in_input_group() == false {
         log::error!("Invoking user is NOT in input group.");
         exit(1);
     }
@@ -42,6 +29,21 @@ pub fn main() {
             Err(error) => log::error!("Unexpected error occured: {}", error),
         }
     }
+}
+
+pub fn user_in_input_group() -> bool {
+    log::trace!("Checking if invoking user is in input group or not.");
+    let groups = unistd::getgroups();
+    for (_, groups) in groups.iter().enumerate() {
+        for group in groups {
+            let group = unistd::Group::from_gid(*group);
+            if group.unwrap().unwrap().name == "input" {
+                log::debug!("Invoking user is in input group.");
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 pub fn check_keyboard(input_path: String) -> bool {
