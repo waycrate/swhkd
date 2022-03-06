3f006d5b7be25d64bed7feef6a1745999381c938
Shinyzenith <aakashsensharma@gmail.com>
2022-03-06 14:45:20 +0530

    [bump] Version & [workflow] upload glibc and musl

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 version=$(awk -F = '/^version/ {print $2}' Cargo.toml | awk '{$1=$1;print}' | tr -d '"')
 make
-zip -r "musl_libc-x86_64-$version.zip" ./bin/swhkd ./bin/swhks
+cd bin
+zip -r "musl_libc-x86_64-$version.zip" swhkd swhks
+cd ..
 make glibc
-zip -r "glibc-x86_64-$version.zip" ./bin/swhkd ./bin/swhks
+cd bin
+zip -r "glibc-x86_64-$version.zip" swhkd swhks
+rm ./swhkd 
+rm ./swhks
