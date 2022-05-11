5edc6ed8f283f614a44e12c9f8da545b0a7d5ff0
Angelo Fallaria <ba.fallaria@gmail.com>
2022-05-11 14:25:06 +0800

    feat: add funcs for UID/GID manipulation

--- src/daemon.rs
+++ src/daemon.rs
@@ -2,7 +2,7 @@ use clap::{arg, Command};
 use evdev::{AttributeSet, Device, InputEventKind, Key};
 use nix::{
     sys::stat::{umask, Mode},
-    unistd::{Group, Uid},
+    unistd::{Gid, Group, Uid},
 };
 use signal_hook_tokio::Signals;
 use std::{
@@ -444,6 +444,78 @@ pub fn fetch_xdg_runtime_path() -> PathBuf {
             log::error!("XDG_RUNTIME_DIR has not been set.");
             Path::new(&format!("/run/user/{}/swhkd.sock", env::var("PKEXEC_UID").unwrap()))
                 .to_path_buf()
+        }
+    }
+}
+
+pub fn getresuid() -> nix::unistd::ResUid {
+    match nix::unistd::getresuid() {
+        Ok(resuid) => resuid,
+        Err(e) => {
+            log::error!("Failed to get RESUID: {:#?}", e);
+            exit(1);
+        }
+    }
+}
+
+pub fn getresgid() -> nix::unistd::ResGid {
+    match nix::unistd::getresgid() {
+        Ok(resgid) => resgid,
+        Err(e) => {
+            log::error!("Failed to get RESGID: {:#?}", e);
+            exit(1);
+        }
+    }
+}
+
+pub fn setresuid(ruid: u32, euid: u32, suid: u32) {
+    let ruid = Uid::from_raw(ruid);
+    let euid = Uid::from_raw(euid);
+    let suid = Uid::from_raw(suid);
+
+    println!("setresuid: {} {} {}", ruid, euid, suid);
+    match nix::unistd::setresuid(ruid, euid, suid) {
+        Ok(_) => log::debug!("Dropping privileges..."),
+        Err(e) => {
+            log::error!("Failed to set RESUID: {:#?}", e);
+            exit(1);
+        }
+    }
+}
+
+pub fn setresgid(rgid: u32, egid: u32, sgid: u32) {
+    let rgid = Uid::from_raw(rgid);
+    let egid = Uid::from_raw(egid);
+    let sgid = Uid::from_raw(sgid);
+
+    println!("setresgid: {} {} {}", rgid, egid, sgid);
+    match nix::unistd::setresuid(rgid, egid, sgid) {
+        Ok(_) => log::debug!("Dropping privileges..."),
+        Err(e) => {
+            log::error!("Failed to set RESGID: {:#?}", e);
+            exit(1);
+        }
+    }
+}
+
+pub fn setinitgroups(user: &nix::unistd::User, gid: u32) {
+    let gid = Gid::from_raw(gid);
+    match nix::unistd::initgroups(&user.gecos, gid) {
+        Ok(_) => log::debug!("Dropping privileges..."),
+        Err(e) => {
+            log::error!("Failed to set init groups: {:#?}", e);
+            exit(1);
+        }
+    }
+}
+
+pub fn setegid(gid: u32) {
+    let gid = Gid::from_raw(gid);
+    match nix::unistd::setegid(gid) {
+        Ok(_) => log::debug!("Dropping privileges..."),
+        Err(e) => {
+            log::error!("Failed to set EGID: {:#?}", e);
+            exit(1);
         }
     }
 }
