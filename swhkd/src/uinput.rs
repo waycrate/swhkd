waycrate/swhkd

commit af0115eb0cbd927a6ba21f5865d9e8489556db22

[uinput] Switch feature added
Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

swhkd/src/uinput.rs
@@ -1,6 +1,6 @@
 use evdev::{
     uinput::{VirtualDevice, VirtualDeviceBuilder},
-    AttributeSet, Key, RelativeAxisType,
+    AttributeSet, Key, RelativeAxisType, SwitchType,
 };
 
 pub fn create_uinput_device() -> Result<VirtualDevice, Box<dyn std::error::Error>> {
@@ -14,10 +14,16 @@ pub fn create_uinput_device() -> Result<VirtualDevice, Box<dyn std::error::Error
         relative_axes.insert(axis);
     }
 
+    let mut switches = AttributeSet::<SwitchType>::new();
+    for switch in get_all_switches() {
+        switches.insert(switch);
+    }
+
     let device = VirtualDeviceBuilder::new()?
         .name("swhkd virtual output")
         .with_keys(&keys)?
         .with_relative_axes(&relative_axes)?
+        .with_switches(&switches)?
         .build()
         .unwrap();
     Ok(device)
@@ -590,5 +596,27 @@ pub fn get_all_relative_axes() -> Vec<RelativeAxisType> {
         RelativeAxisType::REL_RESERVED,
         RelativeAxisType::REL_WHEEL_HI_RES,
         RelativeAxisType::REL_HWHEEL_HI_RES,
+    ]
+}
+
+pub fn get_all_switches() -> Vec<SwitchType> {
+    vec![
+        SwitchType::SW_LID,
+        SwitchType::SW_TABLET_MODE,
+        SwitchType::SW_HEADPHONE_INSERT,
+        SwitchType::SW_RFKILL_ALL,
+        SwitchType::SW_MICROPHONE_INSERT,
+        SwitchType::SW_DOCK,
+        SwitchType::SW_LINEOUT_INSERT,
+        SwitchType::SW_JACK_PHYSICAL_INSERT,
+        SwitchType::SW_VIDEOOUT_INSERT,
+        SwitchType::SW_CAMERA_LENS_COVER,
+        SwitchType::SW_KEYPAD_SLIDE,
+        SwitchType::SW_FRONT_PROXIMITY,
+        SwitchType::SW_ROTATE_LOCK,
+        SwitchType::SW_LINEIN_INSERT,
+        SwitchType::SW_MUTE_DEVICE,
+        SwitchType::SW_PEN_INSERTED,
+        SwitchType::SW_MACHINE_COVER,
     ]
 }

commit 0af3d0df7f781e91393eb084a67b86a3008a9753

[fix] stop rfkill from blocking all radio devices
This fixes a bug introduced in af0115e.

After creating a uinput device with switches support, all radio devices were
blocked. This was due to rfkill reacting to the uinput creation.

We disable rfkill-input (the module reacting to new device creation) to avoid
blocking all radio devices. When a new device (virtual or physical) with the
SW_RFKILL_ALL switch capability bit set appears, rfkill reacts immediately
depending on the value bit. This value bit defaults to unset, which causes
rfkill to use its default eop mode (emergency power off). The uinput API does
not give any way to set the corresponding value bit before creating the device,
and we have no way to avoid rfkill acting upon the device creation or to change
its default mode. Thus, we disable rfkill-input temporarily, hopefully fast
enough that it won't impact anyone. rfkill-input gets enabled automatically
after uinput device creation (actually, when the file gets closed).

swhkd/src/uinput.rs
@@ -3,6 +3,12 @@ use evdev::{
     AttributeSet, Key, RelativeAxisType, SwitchType,
 };
 
+use nix::ioctl_none;
+use std::fs::File;
+use std::os::unix::io::AsRawFd;
+
+ioctl_none!(rfkill_noinput, b'R', 1);
+
 pub fn create_uinput_device() -> Result<VirtualDevice, Box<dyn std::error::Error>> {
     let mut keys = AttributeSet::<Key>::new();
     for key in get_all_keys() {
@@ -29,6 +35,20 @@ pub fn create_uinput_switches_device() -> Result<VirtualDevice, Box<dyn std::err
         switches.insert(switch);
     }
 
+    // We have to disable rfkill-input to avoid blocking all radio devices. When
+    // a new device (virtual or physical) with the SW_RFKILL_ALL capability bit
+    // set appears, rfkill reacts immediately depending on the value bit. This
+    // value bit defaults to unset, which causes rfkill to use its default mode
+    // (which is eop - emergency power off). The uinput API does not give any
+    // way to set the corresponding value bit before creating the device, and we
+    // have no way to avoid rfkill acting upon the device creation or to change
+    // its default mode. Thus, we disable rfkill-input temporarily, hopefully
+    // fast enough that it won't impact anyone. rfkill-input will be enabled
+    // again when the file gets closed.
+    let rfkill_file = File::open("/dev/rfkill")?;
+    unsafe {
+        rfkill_noinput(rfkill_file.as_raw_fd())?;
+    }
     let device = VirtualDeviceBuilder::new()?
         .name("swhkd switches virtual output")
         .with_switches(&switches)?