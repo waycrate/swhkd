33a8d46f6ea2ce49c082df8568cec3c4611d583f
Ivan Dimitrov <ivan@idimitrov.dev>
2024-04-21 14:04:53 +0300

    adding nix default package to flake

--- flake.nix
+++ flake.nix
@@ -1,29 +1,38 @@
 {
   description = "Swhkd devel";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, fenix, ... }:
     let
       pkgsFor = system:
         import nixpkgs {
           inherit system;
-          overlays = [ ];
+          overlays = [ fenix.overlays.default ];
         };
 
       targetSystems = [ "aarch64-linux" "x86_64-linux" ];
-    in {
+    in
+    {
       devShells = nixpkgs.lib.genAttrs targetSystems (system:
         let pkgs = pkgsFor system;
         in {
           default = pkgs.mkShell {
             name = "Swhkd-devel";
             nativeBuildInputs = with pkgs; [
-              # Compilers
-              cargo
-              rustc
-              scdoc
-
+              (pkgs.fenix.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
               # libs
               udev
 
@@ -32,12 +41,30 @@
               clippy
               gdb
               gnumake
-              rust-analyzer
+              rust-analyzer-nightly
               rustfmt
               strace
               valgrind
               zip
             ];
+          };
+        });
+      packages = nixpkgs.lib.genAttrs targetSystems (system:
+        let
+          toolchain = fenix.packages.${system}.minimal.toolchain;
+          pkgs = pkgsFor system;
+        in
+        {
+          default = (pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          }).buildRustPackage {
+            pname = "swhkd";
+            version = "1.2.1";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [ udev ];
           };
         });
     };
