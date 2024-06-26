3ea47512a67eee4544f9ec482fbdfded9ad31e69
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-06-26 10:42:38 +0530

    refactor: remove redundant code, offloading work to parser crate

--- swhkd/src/config.rs
+++ swhkd/src/config.rs
@@ -1,12 +1,10 @@
-use itertools::Itertools;
 use std::collections::HashMap;
-use std::fs;
 use std::{
     fmt,
     path::{Path, PathBuf},
 };
 use sweet::token::KeyAttribute;
-use sweet::SwhkdParser;
+use sweet::{Definition, SwhkdParser};
 
 #[derive(Debug)]
 pub enum Error {
@@ -60,20 +58,13 @@ impl fmt::Display for Error {
     }
 }
 
-pub const IMPORT_STATEMENT: &str = "include";
-pub const UNBIND_STATEMENT: &str = "ignore";
-pub const MODE_STATEMENT: &str = "mode";
-pub const MODE_END_STATEMENT: &str = "endmode";
+/// TODO: implement these in the parser crate
 pub const MODE_ENTER_STATEMENT: &str = "@enter";
 pub const MODE_ESCAPE_STATEMENT: &str = "@escape";
-pub const MODE_SWALLOW_STATEMENT: &str = "swallow";
-pub const MODE_ONEOFF_STATEMENT: &str = "oneoff";
 
 pub fn load(path: &Path) -> Result<Vec<Mode>, Error> {
     let config_self = sweet::SwhkdParser::from(sweet::ParserInput::Path(path)).unwrap();
-    let mut modes: Vec<Mode> = vec![Mode::default()];
-    let mut output = parse_contents(config_self)?;
-    Ok(modes)
+    parse_contents(config_self)
 }
 
 #[derive(Debug, Clone)]
@@ -194,12 +185,23 @@ impl Value for &Hotkey {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Default)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Mode {
     pub name: String,
     pub hotkeys: Vec<Hotkey>,
     pub unbinds: Vec<KeyBinding>,
     pub options: ModeOptions,
+}
+
+impl Default for Mode {
+    fn default() -> Self {
+        Self {
+            name: "normal".to_string(),
+            hotkeys: vec![],
+            unbinds: vec![],
+            options: ModeOptions::default(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Default)]
@@ -208,17 +210,44 @@ pub struct ModeOptions {
     pub oneoff: bool,
 }
 
-impl Mode {
-    pub fn new(name: String) -> Self {
-        Self { name, hotkeys: Vec::new(), unbinds: Vec::new(), options: ModeOptions::default() }
+pub fn parse_contents(contents: SwhkdParser) -> Result<Vec<Mode>, Error> {
+    let mut default_mode = Mode::default();
+
+    for binding in &contents.bindings {
+        default_mode.hotkeys.push(Hotkey {
+            keybinding: sweet_def_to_kb(&binding.definition),
+            command: binding.command.clone(),
+        });
+    }
+    for unbind in contents.unbinds {
+        default_mode.unbinds.push(sweet_def_to_kb(&unbind));
     }
 
-    pub fn default() -> Self {
-        Self::new("normal".to_string())
+    let mut modes = vec![default_mode];
+
+    for sweet::Mode { name, oneoff, swallow, bindings, unbinds } in contents.modes {
+        let mut pushmode =
+            Mode { name, options: ModeOptions { swallow, oneoff }, ..Default::default() };
+        for binding in bindings {
+            let hotkey = Hotkey {
+                keybinding: sweet_def_to_kb(&binding.definition),
+                command: binding.command,
+            };
+            pushmode.hotkeys.retain(|h| h.keybinding != hotkey.keybinding);
+            pushmode.hotkeys.push(hotkey);
+        }
+        for unbind in unbinds {
+            pushmode.unbinds.push(sweet_def_to_kb(&unbind));
+        }
+        modes.push(pushmode);
     }
+    Ok(modes)
 }
 
-pub fn parse_contents(contents: SwhkdParser) -> Result<Vec<Mode>, Error> {
+/// This is a temporary function as glue code between
+/// the SWEET parser and the structures here.
+/// TODO: Bake this into the parser itself.
+fn sweet_def_to_kb(def: &Definition) -> KeyBinding {
     // Don't forget to update valid key list on the man page if you do change this list.
     let key_to_evdev_key: HashMap<&str, evdev::Key> = HashMap::from([
         ("q", evdev::Key::KEY_Q),
@@ -383,259 +412,15 @@ pub fn parse_contents(contents: SwhkdParser) -> Result<Vec<Mode>, Error> {
         ("any", Modifier::Any),
     ]);
 
-    let mut modes: Vec<Mode> = vec![Mode::default()];
-    let mut current_mode: usize = 0;
-
-    for binding in &contents.bindings {
-        let keysym = key_to_evdev_key
-            .get(binding.definition.key.key.to_lowercase().as_str())
-            .cloned()
-            .unwrap();
-        let modifiers = binding
-            .definition
-            .modifiers
-            .iter()
-            .map(|sweet::token::Modifier(modifier)| {
-                mod_to_mod_enum.get(modifier.to_lowercase().as_str()).cloned().unwrap()
-            })
-            .collect();
-        let send = binding.definition.key.attribute == KeyAttribute::Send;
-        let on_release = binding.definition.key.attribute == KeyAttribute::OnRelease;
-        modes[current_mode].hotkeys.push(Hotkey {
-            keybinding: KeyBinding { keysym, modifiers, send, on_release },
-            command: binding.command.clone(),
-        });
-    }
-    for unbind in contents.unbinds {
-        let keysym = key_to_evdev_key.get(unbind.key.key.to_lowercase().as_str()).cloned().unwrap();
-        let modifiers = unbind
-            .modifiers
-            .iter()
-            .map(|sweet::token::Modifier(modifier)| {
-                mod_to_mod_enum.get(modifier.to_lowercase().as_str()).cloned().unwrap()
-            })
-            .collect();
-        let send = unbind.key.attribute == KeyAttribute::Send;
-        let on_release = unbind.key.attribute == KeyAttribute::OnRelease;
-        modes[current_mode].unbinds.push(KeyBinding { keysym, modifiers, send, on_release });
-    }
-
-    // shadowing current_mode
-    for mode in contents.modes.iter() {
-        let mut pushmode = Mode {
-            name: mode.name.clone(),
-            options: ModeOptions { swallow: mode.swallow, oneoff: mode.oneoff },
-            ..Default::default()
-        };
-        for binding in &contents.bindings {
-            let keysym = key_to_evdev_key
-                .get(binding.definition.key.key.to_lowercase().as_str())
-                .cloned()
-                .unwrap();
-            let modifiers = binding
-                .definition
-                .modifiers
-                .iter()
-                .map(|sweet::token::Modifier(modifier)| {
-                    mod_to_mod_enum.get(modifier.to_lowercase().as_str()).cloned().unwrap()
-                })
-                .collect();
-            let send = binding.definition.key.attribute == KeyAttribute::Send;
-            let on_release = binding.definition.key.attribute == KeyAttribute::OnRelease;
-            let hotkey = Hotkey {
-                keybinding: KeyBinding { keysym, modifiers, send, on_release },
-                command: binding.command.clone(),
-            };
-            pushmode.hotkeys.retain(|h| h.keybinding != hotkey.keybinding);
-            pushmode.hotkeys.push(hotkey);
-        }
-        modes.push(pushmode);
-    }
-    Ok(modes)
-}
-
-// We need to get the reference to key_to_evdev_key
-// and mod_to_mod enum instead of recreating them
-// after each function call because it's too expensive
-fn parse_keybind(
-    path: PathBuf,
-    line: &str,
-    line_nr: u32,
-    key_to_evdev_key: &HashMap<&str, evdev::Key>,
-    mod_to_mod_enum: &HashMap<&str, Modifier>,
-) -> Result<KeyBinding, Error> {
-    let line = line.split('#').next().unwrap();
-    let tokens: Vec<String> =
-        line.split('+').map(|s| s.trim().to_lowercase()).filter(|s| s != "_").collect();
-
-    let mut tokens_new = Vec::new();
-    for mut token in tokens {
-        while token.trim().starts_with('_') {
-            token = token.trim().strip_prefix('_').unwrap().to_string();
-        }
-        tokens_new.push(token.trim().to_string());
-    }
-
-    let last_token = tokens_new.last().unwrap().trim();
-
-    // Check if last_token is prefixed with @ or ~ or even both.
-    // If prefixed @, on_release = true; if prefixed ~, send = true
-    let send = last_token.starts_with('~') || last_token.starts_with("@~");
-    let on_release = last_token.starts_with('@') || last_token.starts_with("~@");
-
-    // Delete the @ and ~ in the last token
-    fn strip_at(token: &str) -> &str {
-        token.trim_start_matches(['@', '~'])
-    }
-
-    let last_token = strip_at(last_token);
-    let tokens_no_at: Vec<_> = tokens_new.iter().map(|token| strip_at(token)).collect();
-
-    // Check if each token is valid
-    for token in &tokens_no_at {
-        if key_to_evdev_key.contains_key(token) {
-            // Can't have a keysym that's like a modifier
-            if *token != last_token {
-                return Err(Error::InvalidConfig(ParseError::InvalidModifier(path, line_nr)));
-            }
-        } else if mod_to_mod_enum.contains_key(token) {
-            // Can't have a modifier that's like a keysym
-            if *token == last_token {
-                return Err(Error::InvalidConfig(ParseError::InvalidKeysym(path, line_nr)));
-            }
-        } else {
-            return Err(Error::InvalidConfig(ParseError::UnknownSymbol(path, line_nr)));
-        }
-    }
-
-    // Translate keypress into evdev key
-    let keysym = key_to_evdev_key.get(last_token).unwrap();
-
-    let modifiers: Vec<Modifier> = tokens_no_at[0..(tokens_no_at.len() - 1)]
+    let keysym = key_to_evdev_key.get(def.key.key.to_lowercase().as_str()).cloned().unwrap();
+    let modifiers = def
+        .modifiers
         .iter()
-        .map(|token| *mod_to_mod_enum.get(token).unwrap())
+        .map(|sweet::token::Modifier(modifier)| {
+            mod_to_mod_enum.get(modifier.to_lowercase().as_str()).cloned().unwrap()
+        })
         .collect();
-
-    let mut keybinding = KeyBinding::new(*keysym, modifiers);
-    if send {
-        keybinding = keybinding.send();
-    }
-    if on_release {
-        keybinding = keybinding.on_release();
-    }
-    Ok(keybinding)
-}
-
-pub fn extract_curly_brace(line: &str) -> Vec<String> {
-    if !line.contains('{') || !line.contains('}') || !line.is_ascii() {
-        return vec![line.to_string()];
-    }
-
-    // go through each character in the line and mark the position of each { and }
-    // if a { is not followed by a  }, return the line as is
-    let mut brace_positions: Vec<usize> = Vec::new();
-    let mut flag = false;
-    for (i, c) in line.chars().enumerate() {
-        if c == '{' {
-            if flag {
-                return vec![line.to_string()];
-            }
-            brace_positions.push(i);
-            flag = true;
-        } else if c == '}' {
-            if !flag {
-                return vec![line.to_string()];
-            }
-            brace_positions.push(i);
-            flag = false;
-        }
-    }
-
-    // now we have a list of positions of { and }
-    // we should extract the items between each pair of braces and store them in a vector
-    let mut items: Vec<String> = Vec::new();
-    let mut remaining_line: Vec<String> = Vec::new();
-    let mut start_index = 0;
-    for i in brace_positions.chunks(2) {
-        items.push(line[i[0] + 1..i[1]].to_string());
-        remaining_line.push(line[start_index..i[0]].to_string());
-        start_index = i[1] + 1;
-    }
-
-    // now we have a list of items between each pair of braces
-    // we should extract the items between each comma and store them in a vector
-    let mut tokens_vec: Vec<Vec<String>> = Vec::new();
-    for item in items {
-        // Edge case: escape periods
-        // example:
-        // ```
-        // super + {\,, .}
-        //    riverctl focus-output {previous, next}
-        // ```
-        let item = item.replace("\\,", "comma");
-
-        let items: Vec<String> = item.split(',').map(|s| s.trim().to_string()).collect();
-        tokens_vec.push(handle_ranges(items));
-    }
-
-    fn handle_ranges(items: Vec<String>) -> Vec<String> {
-        let mut output: Vec<String> = Vec::new();
-        for item in items {
-            if !item.contains('-') {
-                output.push(item);
-                continue;
-            }
-            let mut range = item.split('-').map(|s| s.trim());
-
-            let begin_char: &str = if let Some(b) = range.next() {
-                b
-            } else {
-                output.push(item);
-                continue;
-            };
-
-            let end_char: &str = if let Some(e) = range.next() {
-                e
-            } else {
-                output.push(item);
-                continue;
-            };
-
-            // Do not accept range values that are longer than one char
-            // Example invalid: {ef-p} {3-56}
-            // Beginning of the range cannot be greater than end
-            // Example invalid: {9-4} {3-2}
-            if begin_char.len() != 1 || end_char.len() != 1 || begin_char > end_char {
-                output.push(item);
-                continue;
-            }
-
-            // In swhkd we will parse the full range using ASCII values.
-
-            let begin_ascii_val = begin_char.parse::<char>().unwrap() as u8;
-            let end_ascii_val = end_char.parse::<char>().unwrap() as u8;
-
-            for ascii_number in begin_ascii_val..=end_ascii_val {
-                output.push((ascii_number as char).to_string());
-            }
-        }
-        output
-    }
-
-    // now write the tokens back to the line and output a vector
-    let mut output: Vec<String> = Vec::new();
-    // generate a cartesian product iterator for all the vectors in tokens_vec
-    let cartesian_product_iter = tokens_vec.iter().multi_cartesian_product();
-    for tokens in cartesian_product_iter.collect_vec() {
-        let mut line_to_push = String::new();
-        for i in 0..remaining_line.len() {
-            line_to_push.push_str(&remaining_line[i]);
-            line_to_push.push_str(tokens[i]);
-        }
-        if brace_positions[brace_positions.len() - 1] < line.len() - 1 {
-            line_to_push.push_str(&line[brace_positions[brace_positions.len() - 1] + 1..]);
-        }
-        output.push(line_to_push);
-    }
-    output
+    let send = def.key.attribute == KeyAttribute::Send;
+    let on_release = def.key.attribute == KeyAttribute::OnRelease;
+    KeyBinding { keysym, modifiers, send, on_release }
 }
